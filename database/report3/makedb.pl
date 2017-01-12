5bfda9b2d77d1b43f548ad828f01b7aa117fffdd
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-13 08:35:27 +0900

    init git makedb.pl

---
+++ database/report3/makedb.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use utf8;
+use feature qw( say );
+
+use DBI;
+use Time::HiRes qw( usleep gettimeofday tv_interval );
+
+die "Use:$0 arg2 for username  pass" if(@ARGV != 2);
+
+my $dbh = DBI->connect("DBI:mysql:exam2","roo","55taka");
+
+
