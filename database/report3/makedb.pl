8cb000337759039f48874cbf34f7f255c0a7dc29
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-14 22:50:48 +0900

    use String::Random

--- database/report3/makedb.pl
+++ database/report3/makedb.pl
@@ -3,12 +3,13 @@ use strict;
 use warnings;
 use utf8;
 use feature qw( say );
-
+use String::Random;
 use DBI;
 use Time::HiRes qw( usleep gettimeofday tv_interval );
 
 die "Use:$0 arg2 for username  pass" if(@ARGV != 2);
 
-my $dbh = DBI->connect("DBI:mysql:exam2","$ARGV[0]","$ARGV[1]");
+#my $dbh = DBI->connect("DBI:mysql:exam2","$ARGV[0]","$ARGV[1]");
 
-
+my $random = String::Random->new->randregex('[A-M]');
+print "$random$random\n";
