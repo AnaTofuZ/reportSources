5783ef0caead8f7b87acd6a88620ac8772df7f19
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-15 17:09:20 +0900

    add create table sql

--- database/report3/makedb.pl
+++ database/report3/makedb.pl
@@ -9,7 +9,12 @@ use Time::HiRes qw( usleep gettimeofday tv_interval );
 
 die "Use:$0 arg2 for username  pass" if(@ARGV != 2);
 
-#my $dbh = DBI->connect("DBI:mysql:exam2","$ARGV[0]","$ARGV[1]");
+my $dbh = DBI->connect("DBI:mysql:exam2","$ARGV[0]","$ARGV[1]");
 
 my $random = String::Random->new->randregex('[A-M]');
-print "$random$random\n";
+
+my $query = $dbh->prepare("CREATE TABLE noti1(A INT(3000),B CHAR(2),C VARCHAR(6) )");
+
+$query->execute;
+
+
