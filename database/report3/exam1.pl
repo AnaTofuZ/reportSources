022a6342a2081b0310dc8671411839b40401187b
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-17 15:05:26 +0900

    add sort

--- database/report3/exam1.pl
+++ database/report3/exam1.pl
@@ -4,7 +4,8 @@ use warnings;
 use utf8;
 
 use DBI;
-use Time::HiRes;
+use feature qw( say );
+use Time::HiRes qw( usleep gettimeofday tv_interval );
 
 die "Use:$0 arg2 for username  pass" if(@ARGV != 2);
 
@@ -13,3 +14,21 @@ my $dbh = DBI->connect("DBI:mysql:exam2","$ARGV[0]","$ARGV[1]",
         PrintError => 0,
         AutoCommit => 0
     });
+
+my $sth = $dbh->prepare("select * from noti1");
+
+my $result = 0;
+
+for(0..9){
+    my $t0 = [gettimeofday];
+
+    $sth->execute;
+
+    my $elapsed = tv_interval($t0);
+
+    say(int($elapsed * 1000),"ms");
+
+    $result +=(int($elapsed * 1000));
+}
+
+say("Result for",$result/10,"ms");
