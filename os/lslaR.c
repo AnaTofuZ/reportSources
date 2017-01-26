deb55b163da59f228ab5380532d530921bfbc5e6
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-26 18:14:24 +0900

    add roopback

---
+++ os/lslaR.c
@@ -0,0 +1,230 @@
+//
+//
+//  lslaR.c
+//
+//  Created by Takahiro SHIMIZU on 11/15/16.
+//  Copyright © 2016 ie-ryukyu. All rights reserved.
+//
+
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <time.h>
+#include <grp.h>
+#include <stdlib.h>
+#include <string.h>
+#define EROOR 1
+#define FOUND 1
+#define NOT_FOUNT 1
+#define PATH_MAX 4096
+#define TIMES_CHAR 13
+#define LS_L_CHAR 10
+
+void list_dir(char *base_path);
+void puts_list(struct dirent *dp,int *sum);
+void get_detail(mode_t mode,char *get_show);
+char* pathlink(mode_t mode,char* name);
+char* get_username(uid_t uid);
+char* get_groupname(gid_t gid);
+
+int main(int argc, char * argv[]) {
+
+
+    char *path = argc >1 ? argv[1]:".";
+
+    struct dirent *dent;
+
+    list_dir(path);
+
+    return 0;
+}
+
+
+void list_dir(char *base_path){
+
+/**
+ * list_dir is use opendir and reaadir.
+ * print is total block  .
+ */
+
+    DIR *dir;
+    struct dirent *dp;
+    int blocksum = 0;
+    char path[PATH_MAX+1];
+
+    dir = opendir(base_path);
+    
+    if(dir == NULL || chdir(base_path) != 0){
+        perror(base_path);
+        return ;
+     }
+
+    while ((dp = readdir(dir)) !=NULL) {
+         puts_list(dp,&blocksum);
+
+         if(( dp->d_name[0] == '.') && ( dp->d_name[1 + (dp->d_name[1] == '.')] == '\0')){
+             continue;
+         }
+
+         if (dp->d_type == DT_DIR){
+             strncpy(&path[strlen(base_path)],dp->d_name,PATH_MAX - strlen(base_path));
+             printf("\n%s:\n",path);
+            list_dir(path);
+         }
+
+    }
+
+    printf("total %d\n",blocksum);
+
+    closedir(dir);
+}
+
+void puts_list(struct dirent *dp,int *sum){
+
+  /**
+   * print ls elemtns and sum block size caluculation.
+   */
+
+    struct stat sb;
+
+    if(lstat(dp->d_name,&sb) == 0){
+
+    char show[LS_L_CHAR+1];
+    char times[TIMES_CHAR];
+
+    get_detail(sb.st_mode,show);
+    printf("%s  ",show);
+    printf("%2d  ",sb.st_nlink);
+    printf("%s  ",get_username(sb.st_uid));
+    printf("%s  ",get_groupname(sb.st_gid));
+    printf("%6lld  ",sb.st_size);
+    strftime(times,sizeof(times),"%m %e %H:%M",localtime(&sb.st_ctime));
+    printf("%s ",times);
+    printf("%s",dp->d_name);
+
+    char *showlink = pathlink(sb.st_mode,dp->d_name);
+    showlink == NULL ? putchar('\n'):printf(" -> %s\n",showlink);
+
+    *sum +=sb.st_blocks;
+
+    }
+}
+
+void get_detail(mode_t mode,char *get_show){
+/**
+ * get file mode . the entry type,owner permissions,and group permissions.
+ */
+
+    switch (mode & S_IFMT) {
+        case S_IFBLK:
+             get_show[0] ='b';
+             break;
+        case S_IFCHR:
+             get_show[0] = 'c';
+             break;
+        case S_IFDIR:
+             get_show[0] ='d';
+             break;
+        case S_IFLNK:
+             get_show[0]= 'l';
+             break;
+        case S_IFSOCK:
+             get_show[0] ='s';
+             break;
+        case S_IFIFO:
+             get_show[0]='p';
+             break;
+        case S_IFREG:
+             get_show[0]='-';
+             break;
+        defalut:
+             get_show[0]='?';
+             break;
+    }
+
+    get_show[1]  = mode & S_IRUSR ? 'r':'-';
+    get_show[2]  = mode & S_IWUSR ? 'w':'-';
+
+    if (mode & S_ISUID) {
+        get_show[3] = mode & S_IXUSR ? 's':'S';
+    } else {
+        get_show[3] = mode & S_IXUSR ? 'x':'-';
+    }
+
+    get_show[4]  = mode & S_IRGRP ? 'r':'-';
+    get_show[5]  = mode & S_IWGRP ? 'w':'-';
+
+    if (mode & S_ISGID) {
+        get_show[6] = mode & S_IXGRP ? 's':'S';
+    } else {
+        get_show[6] = mode & S_IXGRP ? 'x':'-';
+    }
+
+    get_show[7]  = mode & S_IROTH ? 'r':'-';
+    get_show[8]  = mode & S_IWOTH ? 'w':'-';
+
+    if (mode & S_ISVTX) {
+        get_show[9] = mode & S_IXOTH ? 't':'T';
+    } else {
+        get_show[9] = mode & S_IXOTH ? 'x':'-';
+    }
+    
+    get_show[10] ='\0';
+
+
+}
+
+/*
+ * get_* is using *id get to *name
+ */
+
+char* get_username(uid_t uid){
+
+    struct passwd *passwd = getpwuid(uid);
+
+    if (passwd != NULL) {
+        return passwd->pw_name;
+    } else {
+        char* username;
+        sprintf(username,"%d",uid);
+        return username;
+    }
+}
+
+char* get_groupname(gid_t gid){
+    struct group *group = getgrgid(gid);
+
+    if(group !=NULL){
+        return group->gr_name;
+    } else {
+        char* groupname;
+        sprintf(groupname,"%d",gid); 
+        return groupname;
+    }
+}
+
+/*
+ * pathlink show symblolick link
+ */
+
+char* pathlink(mode_t mode,char* name){
+    char* returnLink =NULL;
+
+    if (S_ISLNK(mode)){
+        char link[PATH_MAX +1];
+        int link_len = readlink(name,link,PATH_MAX);
+
+        if (link_len >0) {
+            link[link_len]='\0';
+            returnLink = link;
+        }
+    }
+    return returnLink;
+}
+
+
