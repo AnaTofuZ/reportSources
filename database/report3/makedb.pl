8485b0e92211b4768dccfc976154a1042ed2fadd
AnaTofuZ <e155730@ie.u-ryukyu.ac.jp>
2017-04-26 11:57:48 +0900

    modofied database

--- database/report3/makedb.pl
+++ database/report3/makedb.pl
@@ -9,7 +9,7 @@ use Time::HiRes qw( usleep gettimeofday tv_interval );
 
 die "Use:$0 arg2 for username  pass" if(@ARGV != 2);
 
-my $dbh = DBI->connect("DBI:mysql:exam2","$ARGV[0]","$ARGV[1]");
+my $dbh = DBI->connect("DBI:mysql:exam3","$ARGV[0]","$ARGV[1]");
 
 my $random = String::Random->new->randregex('[A-M]');
 
@@ -20,7 +20,7 @@ my @sex =qw[ male female];
 
 my $sth =$dbh->prepare("INSERT INTO noti1 VALUES(?,?,?)");
 
-for  (1..10000000){
+for  (1..2000000){
     my $randomNum = int(rand(3000)) +1;
 
     my $randomChar = String::Random->new->randregex('[A-M]');
