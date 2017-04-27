57dd7a9d1306fe9ba31fdd9ddb932d9caace829e
AnaTofuZ <e155730@ie.u-ryukyu.ac.jp>
2017-04-27 21:22:44 +0900

    d関数の結果まで作成

--- pattern/report3/GP.c
+++ pattern/report3/GP.c
@@ -11,7 +11,6 @@
 #include <string.h>
 
 void usage(void);
-int g(int,int);
 int d(char,char);
 
 int main(int ac,char *av[]){
@@ -22,25 +21,64 @@ int main(int ac,char *av[]){
         return 1;
     }
 
-    // 各引数(文字列)をInpt1,Inp2に設定
-    char* Inp1 = av[1];
-    char* Inp2 = av[2];
+    // 各引数(文字列)をI_A,J_Aに設定
+    char* I_A = av[1];
+    char* J_A = av[2];
+
 
    //各文字列の文字数をI,Jに設定
-    int I = strlen(Inp1);
-    int J = strlen(Inp2);
+    int I = strlen(I_A);
+    int J = strlen(J_A);
 
-    printf("%d\n",d(Inp1[0],Inp2[0]));
 
-    printf("%d\n",I);
 
+    // 2次元配列D[I][J]を作成
+
+    int D[I][J];
+
+    // 2次元配列D[I][J]に関数dの返り値を設定
+
+    for (int i = 0; i < I; i++) {
+        for (int j = 0; j < J; j++) {
+                D[i][j]=d(I_A[i],J_A[j]);
+        }
+    }
+
+
+    // 確認用としてDの結果を票形式で出力
+
+    puts("d(i,j)");
+
+    for (int i = --I; 0 <= i ; i--) {
+        for (int j =0; j < J ; j++) {
+                printf("%3d",D[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    // 2次元配列g[I][J]を作成
+
+    int g[I][J];
+
+    // 横一列のみ初期設定
 
     for (int j = 0; j < J; j++) {
+        
+        if(j==0){
+            g[0][0] = 2 * D[0][0];
+        }else{
+            int i=--j;
+            g[0][j] = g[0][i] + D[0][j];
+        }
+    }
 
-        for (int i = 0; i < I; i++) {
+    for (int i = 1; i < I; i++) {
+        for (int j = 0; j < J; j++) {
             
         }
     }
+
     return 0;
 }
 
@@ -51,20 +89,17 @@ void usage(void){
     puts("Usage> ./GP abcde abfeg");
 }
 
-int g(int i,int j){
-
-    return 0;
-}
 
 //関数d
 //対象とする2文字が等しければ0
 //そうでなければ1を返す.
 
 int d(char i,char j){
-    
+
     if (i == j){
-        return 0;
+        return (0);
     }else{
-        return 1;
+        return (1);
     }
+
 }
