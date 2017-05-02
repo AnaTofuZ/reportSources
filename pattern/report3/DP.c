31e0e5dc5287b6a528711673377e502a25b01ef9
AnaTofuZ <e155730@ie.u-ryukyu.ac.jp>
2017-05-02 12:09:41 +0900

    make sel

--- pattern/report3/DP.c
+++ pattern/report3/DP.c
@@ -10,8 +10,11 @@
 #include <stdio.h>
 #include <string.h>
 
+#define ARRAY_MAX 255
+
 void usage(void);
 int d(char,char);
+void showArray(int,int,int ar[ARRAY_MAX][ARRAY_MAX],char*,char*);
 
 int main(int ac,char *av[]){
 
@@ -25,12 +28,9 @@ int main(int ac,char *av[]){
     char* I_A = av[1];
     char* J_A = av[2];
 
-
    //各文字列の文字数をI,Jに設定
     int I = strlen(I_A);
     int J = strlen(J_A);
-
-
 
     // 2次元配列D[I][J]を作成
 
@@ -49,12 +49,7 @@ int main(int ac,char *av[]){
 
     puts("d(i,j)");
 
-    for (int i = (I-1); 0 <= i ; i--) {
-        for (int j =0; j < J ; j++) {
-                printf("%3d",D[i][j]);
-        }
-        printf("\n");
-    }
+    showArray(I,J,D,I_A,J_A);
 
 
     // 2次元配列g[I][J]を作成
@@ -80,21 +75,15 @@ int main(int ac,char *av[]){
         for (int j = 0; j < J; j++) {
 
         // 1番左の列で無いかどうかの判断
-
             if (j !=0) {
 
-
                 //現段階の最小値は左と足したもの
-
                int  min = g[i-1][j]+D[i][j];
-
                 // 左斜め下が最小値の場合,そちらを採用
-
                 if(min > g[i-1][j-1]+ 2*D[i][j]){
                     min = g[i-1][j-1]+ 2*D[i][j];
 
                 // 下が最小値の場合,そちらを採用
-
                     if (min > g[i][j-1] + D[i][j]){
                         g[i][j] = g[i][j-1] + D[i][j];
                         continue;
@@ -104,7 +93,6 @@ int main(int ac,char *av[]){
                 }
                 
                 g[i][j]=min;
-
             } else {
                 // 1番左の列の場合1つ下のものとの距離を計算
                  g[i][0] = g[i-1][0]+D[i][0]; 
@@ -114,13 +102,7 @@ int main(int ac,char *av[]){
     }
 
     puts("g(i,j)");
-
-    for (int i = (I-1); 0 <= i ; i--) {
-        for (int j =0; j < J ; j++) {
-                printf("%3d",g[i][j]);
-        }
-        printf("\n");
-    }
+    showArray(I,J,g,I_A,J_A);
 
     puts("-----");
     printf("This words diff :%3d\n",g[I-1][J-1]);
@@ -150,3 +132,34 @@ int d(char i,char j){
 
 }
 
+// showArray関数
+// 多次元配列を表の形式で出力
+
+void showArray(int I,int J,int ar[I][J],char* I_A,char* J_A){
+
+    for (int i = (I-1); 0 <= i ; i--) {
+    
+        printf("%c|",I_A[i]);
+
+        for (int j =0; j < J ; j++) {
+                printf("%3d",ar[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf(" ");
+
+    for (int j = 0; j < J; j++) {
+        printf("---");
+    }
+
+    puts("");
+    printf("  ");
+
+    for (int j = 0; j < J; j++) {
+        printf("%3c",J_A[j]);
+    }
+
+    printf("\n");
+
+}
